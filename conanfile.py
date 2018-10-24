8dba7a36787a60e046d2ce7aed04e93a933b3aeb
Guillaume Delbergue <guillaume.delbergue@hiventive.com>
2018-10-24 22:01:24 +0200

    update cmake
    Signed-off-by: Guillaume Delbergue <guillaume.delbergue@hiventive.com>

--- conanfile.py
+++ conanfile.py
@@ -1,23 +1,22 @@
 from conans import ConanFile, CMake, tools
-from conans.util import files
 import yaml
 
-class CommunicationConan(ConanFile):
-    name = "communication"
+class HVCommunicationConan(ConanFile):
+    name = str(yaml.load(tools.load("settings.yml"))['conan']['name'])
     version = str(yaml.load(tools.load("settings.yml"))['project']['version'])
     license = "MIT"
     url = "https://github.com/hiventive/hvcommunication"
-    description = "Communication"
+    author = "Hiventive"
+    description = "SystemC/TLM communication library"
     settings = "cppstd", "os", "compiler", "build_type", "arch"
     options = {"shared": [True, False], "fPIC": [True, False], "fPIE": [True, False]}
     default_options = "shared=False", "fPIC=False", "fPIE=False"
     generators = "cmake"
     exports = "settings.yml"
-    exports_sources = "src/*", "CMakeLists.txt"
-    requires = "gtest/1.8.0@bincrafters/stable", \
-               "systemc/[~2.3.2]@hiventive/stable", \
-               "configuration/0.1.0@hiventive/testing", \
-               "common/0.2.0@hiventive/testing"
+    exports_sources = "src/*", "CMakeLists.txt", "cmake/*"
+    requires = "gtest/1.8.0@hiventive/stable", \
+               "hvconfiguration/0.2.0@hiventive/testing", \
+			   "hvcommon/0.3.0@hiventive/testing"
 
     def _configure_cmake(self):
         cmake = CMake(self)
@@ -33,9 +32,6 @@ class CommunicationConan(ConanFile):
     def package(self):
         cmake = self._configure_cmake()
         cmake.install()
-        self.copy("HVCommunication", dst="include", src="src")
-        self.copy("*.h", dst="include", src="src") # FIXME
-        self.copy("*.hpp", dst="include", src="src")
-                
+
     def package_info(self):
-        self.cpp_info.libs = ["communication"]
+        self.cpp_info.libs = ["hvcommunication"]
