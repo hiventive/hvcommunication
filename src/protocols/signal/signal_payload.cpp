4bddbf91111bf6b34916173bfc2b57b95bcf2d8d
Benjamin Barrois <benjamin.barrois@hiventive.com>
2019-03-20 13:36:38 +0000

    ✨ Add tristate signal payload

--- src/protocols/signal/signal_payload.cpp
+++ src/protocols/signal/signal_payload.cpp
@@ -17,62 +17,63 @@ namespace tlm2 {
 namespace protocols {
 namespace signal {
 
-SignalPayload::SignalPayload() :
-		BasePayload(), mSignalValue(false), mResponseStatus(SIGNAL_OK_RESPONSE) {
+SignalPayload::SignalPayload()
+    : BasePayload(), mSignalValue(false), mResponseStatus(SIGNAL_OK_RESPONSE) {
 }
 
-SignalPayload::SignalPayload(MemoryManagementIf* mm_) :
-		BasePayload(mm_), mSignalValue(false), mResponseStatus(
-				SIGNAL_OK_RESPONSE) {
+SignalPayload::SignalPayload(MemoryManagementIf *mm_)
+    : BasePayload(mm_), mSignalValue(false), mResponseStatus(SIGNAL_OK_RESPONSE) {
 }
 
 SignalPayload::~SignalPayload() {
 }
 
 // Accessors
-bool SignalPayload::getValue() const {
-	return mSignalValue;
+hv_signal_t SignalPayload::getValue() const {
+    return mSignalValue;
 }
 
 SignalResponseStatus SignalPayload::getResponseStatus() const {
-	return mResponseStatus;
+    return mResponseStatus;
 }
 
 // Modifiers
-void SignalPayload::setValue(const bool& sigVal_) {
-	mSignalValue = sigVal_;
+void SignalPayload::setValue(const hv_signal_t &sigVal_) {
+    mSignalValue = sigVal_;
 }
 
-void SignalPayload::setResponseStatus(
-		const SignalResponseStatus& responseStatus_) {
-	mResponseStatus = responseStatus_;
+void SignalPayload::setValue(const bool &sigVal_) {
+    mSignalValue = sigVal_ ? 1 : 0;
+}
+
+void SignalPayload::setResponseStatus(const SignalResponseStatus &responseStatus_) {
+    mResponseStatus = responseStatus_;
 }
 
 // Helpers
 bool SignalPayload::isResponseOk() const {
-	return this->mResponseStatus > 0;
+    return this->mResponseStatus > 0;
 }
 
 bool SignalPayload::isResponseError() const {
-	return !this->mResponseStatus;
+    return !this->mResponseStatus;
 }
 
 ::std::string SignalPayload::getResponseString() const {
-	std::string ret;
-	switch (mResponseStatus) {
-	case SIGNAL_OK_RESPONSE:
-		ret = "SIGNAL_OK_RESPONSE";
-		break;
-	default:
-		ret = "SIGNAL_ERROR_RESPONSE";
-		break;
-	}
-	return ret;
+    std::string ret;
+    switch (mResponseStatus) {
+    case SIGNAL_OK_RESPONSE:
+        ret = "SIGNAL_OK_RESPONSE";
+        break;
+    default:
+        ret = "SIGNAL_ERROR_RESPONSE";
+        break;
+    }
+    return ret;
 }
 
-}
-}
-}
-}
-}
-
+} // namespace signal
+} // namespace protocols
+} // namespace tlm2
+} // namespace communication
+} // namespace hv
--- src/protocols/signal/signal_payload.h
+++ src/protocols/signal/signal_payload.h
@@ -22,46 +22,49 @@ namespace tlm2 {
 namespace protocols {
 namespace signal {
 
-enum SignalResponseStatus {
-	SIGNAL_OK_RESPONSE = 1, SIGNAL_ERROR_RESPONSE = 0
+// FIXME: move to common?
+typedef ::hv::common::hvuint8_t hv_signal_t;
+
+enum SignalState : hv_signal_t { SIGNAL_DOWN = 0, SIGNAL_UP = 1, SIGNAL_UNSET = 2 };
+
+enum SignalResponseStatus { SIGNAL_OK_RESPONSE = 1, SIGNAL_ERROR_RESPONSE = 0 };
+
+class SignalPayload : public BasePayload {
+  public:
+    SignalPayload();
+    explicit SignalPayload(MemoryManagementIf *);
+
+    virtual ~SignalPayload();
+
+    // Accessors
+    hv_signal_t getValue() const;
+    SignalResponseStatus getResponseStatus() const;
+
+    // Modifiers
+    void setValue(const hv_signal_t &);
+    void setValue(const bool &);
+    void setResponseStatus(const SignalResponseStatus &);
+
+    // Helpers
+    bool isResponseOk() const;
+    bool isResponseError() const;
+    ::std::string getResponseString() const override;
+
+  private:
+    // Deleting copy ctor and assignment operator
+    // Note: they are set private and non-implemented instead for compatibility
+    SignalPayload(const SignalPayload &) /* = delete */;
+    SignalPayload &operator=(const SignalPayload &src) /* = delete */;
+
+  protected:
+    hv_signal_t mSignalValue;
+    SignalResponseStatus mResponseStatus;
 };
 
-class SignalPayload: public BasePayload {
-public:
-	SignalPayload();
-	explicit SignalPayload(MemoryManagementIf*);
-
-	virtual ~SignalPayload();
-
-	// Accessors
-	bool getValue() const;
-	SignalResponseStatus getResponseStatus() const;
-
-	// Modifiers
-	void setValue(const bool&);
-	void setResponseStatus(const SignalResponseStatus&);
-
-	// Helpers
-	bool isResponseOk() const;
-	bool isResponseError() const;
-	::std::string getResponseString() const override;
-
-private:
-	// Deleting copy ctor and assignment operator
-	// Note: they are set private and non-implemented instead for compatibility
-	SignalPayload(const SignalPayload&) /* = delete */;
-	SignalPayload& operator =(const SignalPayload& src) /* = delete */;
-
-protected:
-	bool mSignalValue;
-	SignalResponseStatus mResponseStatus;
-
-};
-
-}
-}
-}
-}
-}
+} // namespace signal
+} // namespace protocols
+} // namespace tlm2
+} // namespace communication
+} // namespace hv
 
 #endif /* HV_PROTOCOLS_SIGNAL_SIGNAL_PAYLOAD_H_ */
